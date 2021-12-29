3cff35b552ca3ac77501af3f6b26987d1c178903
Ammad Hassan <emo.rajput@yahoo.com>
2021-12-29 15:23:08 +0500

    Header section responsive CSS

--- app.js
+++ app.js
@@ -16,3 +16,24 @@ function dropdownReset()
     var x = document.getElementById("menu");
     x.className = "";
 }
+
+
+
+function booking()
+{
+    let arrivalCity = document.getElementById("arrival_city");
+    let departureCity = document.getElementById("departure_city");
+    console.log(arrivalCity);
+    console.log(departureCity);
+
+    if(arrivalCity !== departureCity)
+    {
+        alert("Your booking is confirmed");
+    
+    }
+    else
+    {
+        alert("You cannot select same Departure City or Arrival City");
+    }
+}
+
